1efa205781c042172ac2b8b5d8ad501f5dbc4a87
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-06-20 11:54:25 -0300

    volta de arquivos

---
+++ pages/formi1.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+
+  
+  function App() {
+    const formik = useFormik({
+      initialValues: {
+        userEmail: 'email@email.com',
+        userPassword: '123456',
+      },
+    });
+  
+    return (
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        console.log(formik.values);
+        alert('Olha o console!');
+      }}
+      >
+        <div className="formField">
+          <label htmlFor="userEmail">
+            E-mail:
+          </label>
+          <input
+            type="text"
+            placeholder="email@example.com"
+            name="userEmail"
+            id="userEmail"
+            onChange={formik.handleChange}
+            value={formik.values.userEmail}
+          />
+          {/* <span class="formField__error">This field is required</span> */}
+        </div>
+  
+        <div className="formField">
+          <label htmlFor="userPassword">
+            Password:
+          </label>
+          <input
+            type="password"
+            placeholder="Your secret password"
+            name="userPassword"
+            id="userPassword"
+            onChange={formik.handleChange}
+            value={formik.values.userPassword}
+          />
+          {/* <span class="formField__error">This field is required</span> */}
+        </div>
+  
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    );
+  }
+  
+  export default App;
